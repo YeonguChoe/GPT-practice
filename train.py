79aae64aa5e60e6315c91c84c0a9a2703633fcc5
Yeongu Choe <yeonguchoe@Yeongus-MacBook-Air.local>
2025-10-25 15:09:53 -0400

    adding

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import torch
 
 f = open("input.txt", "r", encoding="utf-8")
 text = f.read()
+text_length = len(text)
 
 
 # Encoder
@@ -22,5 +23,8 @@ def decode(l):
 
 data = torch.tensor(encode(text), dtype=torch.int32)
 
+training_data = data[:text_length]
+validation_data = data[text_length:]
 
-print(len(text))
+context_window = 8
+x = training_data[:context_window + 1]
